ea452f7a2f7e19c45ce9287664ed2865094b58fd
CodeSlinger <101233254+dhanubalde@users.noreply.github.com>
2024-03-13 09:53:13 +0800

    Update page.tsx

--- Next Layout/conditional layout/login/page.tsx
+++ Next Layout/conditional layout/login/page.tsx
@@ -1,14 +1,13 @@
-// import Link from 'next/link'
-// import React from 'react'
+ import Link from 'next/link'
+ import React from 'react'
 
-// const page = () => {
-//   return (
-//     <div>
-//       <hr />
-//       <h1>Login page</h1>
-//       <Link href={`/`}>Logout</Link>
-//     </div>
-//   )
-// }
-
-// export default page
+const page = () => {
+return (
+   <div>
+     <hr />
+     <h1>Login page</h1>
+     <Link href={`/`}>Logout</Link>
+   </div>
+  ) 
+}
+ export default page
